ac811a388392484547badac275557288c5c57c0d
Zero <zero@pop-os.localdomain>
2020-01-08 13:37:54 +0100

    Added login form template in home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleguideComponent } from './pages/exampleguide/exampleguide.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginModalComponent } from './forms/login-modal/login-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExampleguideComponent,
-    HomeComponent
+    HomeComponent,
+    LoginModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
