a91923487c558eb6fbbed22dc857b57271118350
Zero <zero@pop-os.localdomain>
2020-01-21 14:31:47 +0100

    Created Guide Maker frontend.

--- src/app/guide-maker/guide-maker.component.ts
+++ src/app/guide-maker/guide-maker.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guide-maker.component.css']
 })
 export class GuideMakerComponent implements OnInit {
+  step_count = [1];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addStep() {
+    this.step_count.push(this.step_count[this.step_count.length - 1] + 1);
+  }
+
+  clearSteps() {
+    this.step_count = [];
+    this.step_count = [1];
   }
 
 }
